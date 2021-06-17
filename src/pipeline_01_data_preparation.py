41467b06ceac7f5b3a777f4f515856797c9d1e6e
unknown <bhardwajsiddhant03@gmail.com>
2021-06-17 13:23:30 +0530

    read Configuration

--- src/pipeline_01_data_preparation.py
+++ src/pipeline_01_data_preparation.py
@@ -4,10 +4,22 @@ from typing import DefaultDict
 import yaml
 import logging
 
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+
+def main(config_path,datasource):
+    config = read_params(config_path)
+    print(config)
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
-    args.add_argument("--config",default="default")
+    optimal_path = str(os.getcwd())
+    optimal_path = '\\'.join(optimal_path.split('\\')[:-1])
+    default_config_path = optimal_path + '\\' + os.path.join("config","params.yaml")
+    args.add_argument("--config",default=default_config_path)
     args.add_argument("--datasource",default = None)
     parsed_args = args.parse_args()
-    print(parsed_args.config,parsed_args.datasource)
+    main(config_path = parsed_args.config,datasource = parsed_args.datasource)
